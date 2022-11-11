b51ba344e9646ac5d5130e8ee2b3f8f1bacf100f
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-11-11 15:22:37 +0100

    used setUpSettingsCubit in the cubit tests

--- test/settings_cubit_test.dart
+++ test/settings_cubit_test.dart
@@ -1,8 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:stoing_settings_with_hydrated_bloc/settings.dart';
 import 'package:stoing_settings_with_hydrated_bloc/settings_cubit.dart';
 
@@ -10,18 +8,9 @@ import 'mocks.dart';
 
 void main() {
   group('SettingsCubit', () {
-    late Storage storage;
-
-    setUp(() {
-      storage = MockStorage();
-      when(() => storage.write(any(), any() as dynamic)).thenAnswer(
-        (_) async {},
-      );
-      HydratedBloc.storage = storage;
-    });
+    setUp(setUpSettingsCubit);
 
     test('initial state theme is system mode', () {
-      final settingsCubit = SettingsCubit();
       expect(settingsCubit.state.themeMode, equals(ThemeMode.system));
     });
 
@@ -40,16 +29,15 @@ void main() {
 
     group('json (de)serialization', () {
       test('fromJson converts from json to state', () {
-        final settingsCubit = SettingsCubit();
         final result = settingsCubit.fromJson({'themeMode': 0});
         expect(result, const Settings(themeMode: ThemeMode.system));
       });
     });
 
     test('toJson converts from state to json', () {
-      final settingsCubit = SettingsCubit();
-      final result =
-          settingsCubit.toJson(const Settings(themeMode: ThemeMode.system));
+      final result = settingsCubit.toJson(
+        const Settings(themeMode: ThemeMode.system),
+      );
       expect(result, {'themeMode': 0});
     });
   });
